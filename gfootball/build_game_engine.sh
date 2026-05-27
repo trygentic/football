269533746ee210e39d7c9fb55b7757f035e91278
agentloop <agentloop@local>
2026-05-27 00:14:32 -0400

    try-brew-sdl: link against brew SDL2 instead of conda SDL2

--- gfootball/build_game_engine.sh
+++ gfootball/build_game_engine.sh
@@ -41,6 +41,7 @@ pushd third_party/gfootball_engine && cmake . \
     -DPython3_EXECUTABLE="$PYTHON_EXE" \
     -DPython_ROOT_DIR="$PYTHON_PREFIX" \
     -DPython3_ROOT_DIR="$PYTHON_PREFIX" \
-    -DCMAKE_PREFIX_PATH="$PYTHON_PREFIX" \
+    -DCMAKE_PREFIX_PATH="/opt/homebrew;$PYTHON_PREFIX" \
+    -DSDL2_DIR=/opt/homebrew/lib/cmake/SDL2 \
     && make -j $PARALLELISM && popd
 pushd third_party/gfootball_engine && ln -sf libgame.$LIB_EXTENSION _gameplayfootball.so && popd
